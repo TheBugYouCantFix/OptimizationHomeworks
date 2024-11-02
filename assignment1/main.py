b32e747f6fcac4492281c96083eea5a8c5e82817
alexg <alex.gorbanev05@gmail.com>
2024-11-02 18:35:22 +0500

    fixed input style and wrapped into a function

--- assignment1/main.py
+++ assignment1/main.py
@@ -43,69 +43,76 @@ Input examlpe 3
 
 Note: all problems here are assumed to be maximization problems with "<=" signs in the constraints 
 """
-n_xs = int(input())
-z = list(map(int, input().split()))
 
-# 1, 2. Converting to the standard form and building the table
-n_constraints = int(input())
-z = list(map(lambda x: -x, z)) + [0 for _ in range(n_constraints + 1)] # slack values are 0, last value is right hand side (solution) which is initailly 0
-# also make every coefficient of z negative as we transfer it to the left hand side
-constraints = []
-basic_vars = [-1 for _ in range(n_xs + n_constraints)]
+def simplex_method():
+    n_xs = int(input())
+    z = list(map(int, input().split()))
 
-for i in range(n_constraints):
-    slacks = [0 for _ in range(n_constraints)]
-    slacks[i] = 1
-    c = list(map(int, input().split()))
-    c = c[:-1] + slacks + [c[-1]] 
-    constraints.append(c)
+    # 1, 2. Converting to the standard form and building the table
+    n_constraints = int(input())
+    z = list(map(lambda x: -x, z)) + [0 for _ in range(n_constraints + 1)] # slack values are 0, last value is right hand side (solution) which is initailly 0
+    # also make every coefficient of z negative as we transfer it to the left hand side
+    constraints = []
+    for i in range(n_constraints):
+        constraints.append(list(map(int, input().split())))
 
-# checking whether the right hand side values are non-negative
-for i in constraints:
-    if i[-1] <= 0:
-        print("The method is not applicable!")
-        exit(0)
+    basic_vars = [-1 for _ in range(n_xs + n_constraints)]
 
-table = [z] + constraints
-while any(map(lambda x: x < 0, table[0])):
-    # 3, 4. Find the smallest positive ratio
+    b = list(map(int, input().split()))
 
-    # Select the most negative term in the first row
-    
-    entering_var_index = table[0].index(min(table[0]))
+    for i in range(n_constraints):
+        slacks = [0 for _ in range(n_constraints)]
+        slacks[i] = 1
+        c = constraints[i] + [b[i]]
+        c = c[:-1] + slacks + [c[-1]] 
+        constraints[i] = c
 
-    min_pos_ratio = 10**9
-    leaving_var_index = 0
+    # checking whether the right hand side values are non-negative
+    for i in constraints:
+        if i[-1] <= 0:
+            print("The method is not applicable!")
+            exit(0)
 
-    for i in range(len(table)):
-        if table[i][entering_var_index] == 0:
-            continue
+    table = [z] + constraints
+    while any(map(lambda x: x < 0, table[0])):
+        # 3, 4. Find the smallest positive ratio
 
-        ratio = table[i][-1] / table[i][entering_var_index]
+        # Select the most negative term in the first row
+        
+        entering_var_index = table[0].index(min(table[0]))
 
-        if ratio > 0 and ratio < min_pos_ratio:
-            min_pos_ratio = ratio
-            leaving_var_index = i
+        min_pos_ratio = 10**9
+        leaving_var_index = 0
 
-    # 5. dividing the key row by the key element
-    key_element = table[leaving_var_index][entering_var_index]
-    for i in range(len(table[leaving_var_index])):
-        table[leaving_var_index][i] /= key_element
+        for i in range(len(table)):
+            if table[i][entering_var_index] == 0:
+                continue
 
-    # 6. Make the key column to zero by substracting key row from other rows
-    for i in range(len(table)):
-        if i == leaving_var_index:
-            continue
+            ratio = table[i][-1] / table[i][entering_var_index]
 
-        coefficient = -table[i][entering_var_index] / table[leaving_var_index][entering_var_index]
-        for j in range(len(z)):
-            table[i][j] += coefficient * table[leaving_var_index][j]
+            if ratio > 0 and ratio < min_pos_ratio:
+                min_pos_ratio = ratio
+                leaving_var_index = i
 
-    # 7. Change the basic varibale of the key row with the name of key column
-    basic_vars[entering_var_index] = leaving_var_index
+        # 5. dividing the key row by the key element
+        key_element = table[leaving_var_index][entering_var_index]
+        for i in range(len(table[leaving_var_index])):
+            table[leaving_var_index][i] /= key_element
+
+        # 6. Make the key column to zero by substracting key row from other rows
+        for i in range(len(table)):
+            if i == leaving_var_index:
+                continue
+
+            coefficient = -table[i][entering_var_index] / table[leaving_var_index][entering_var_index]
+            for j in range(len(z)):
+                table[i][j] += coefficient * table[leaving_var_index][j]
+
+        # 7. Change the basic varibale of the key row with the name of key column
+        basic_vars[entering_var_index] = leaving_var_index
 
 
-print(f'z={table[0][-1]}', end='')
-for i in range(n_xs):
-    if basic_vars[i] != -1:
-        print(f', x{i + 1}={table[basic_vars[i]][-1]}', end='')
+    print(f'z={table[0][-1]}', end='')
+    for i in range(n_xs):
+        if basic_vars[i] != -1:
+            print(f', x{i + 1}={table[basic_vars[i]][-1]}', end='')
